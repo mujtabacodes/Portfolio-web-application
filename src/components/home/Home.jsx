4b31c068e7b39d058319f41dcca0f1999ba4292f
Mujtaba Ali <mujiroy01@gmail.com>
2022-08-04 21:41:45 +0500

    Header is complete

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import './home.css'
-
-
+import React from "react";
+import CTA from "./CTA";
+import "./home.css";
+// import ME from './../../assets/images/home-img.jpg'
+import ME from './../../assets/images/header-img.png'
+import HomeSocial from "./HomeSocial";
 export default function Home() {
   return (
     <>
-      <h5>Hello I'm</h5>
-      <h1>Mujtaba Shafique</h1>
-      <h5>Software Engineer | Full stack developer | MERN | JAVA(springboot)</h5>
+      <header className="container home__container">
+        <h5>Hello I'm</h5>
+        <h1>Mujtaba Shafique</h1>
+        <h5 className="text-light">
+          Software Engineer | Full stack developer | MERN | JAVA(springboot)
+        </h5>
+        <CTA/>
+       <HomeSocial/>
+        <div className="me">
+          <img src={ME} alt="" className="img"/>
+        </div>
+        <a href="#contact" className="scroll__down">Scroll Down</a>
+      </header>
     </>
-  )
+  );
 }
---
+++ src/components/home/HomeSocial.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {AiFillGithub} from 'react-icons/ai'
+
+export default function HomeSocial() {
+  return (
+    <>
+   <div className="header__socials">
+    <a href="https://www.linkedin.com/in/mujtaba-shafique/" target='_blank'><BsLinkedin/></a>
+    <a href="https://github.com/Mujtaba4li" target='_blank'><AiFillGithub/></a>
+    </div>
+    </>
+  )
+}
