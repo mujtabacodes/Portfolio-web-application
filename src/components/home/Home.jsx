156f0dd98d4274c24c9df64373d610e30061684d
Mujtaba Ali <mujiroy01@gmail.com>
2022-08-04 16:21:37 +0500

    Working on header

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import './home.css'
+
+
 export default function Home() {
   return (
-    <div >Home</div>
+    <>
+      <h5>Hello I'm</h5>
+      <h1>Mujtaba Shafique</h1>
+      <h5>Software Engineer | Full stack developer | MERN | JAVA(springboot)</h5>
+    </>
   )
 }
