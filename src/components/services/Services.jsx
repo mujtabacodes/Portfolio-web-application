87f8c37893ce077c7e73668d2e66e11f8f3379c8
Mujtaba Ali <mujiroy01@gmail.com>
2022-08-05 22:45:55 +0500

    HTML completed for Services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,146 @@
 import React from 'react'
-import './services.css'
+import './services.css';
+import {BsCheck} from 'react-icons/bs'
+
 export default function Services() {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className='service'>
+          <div className="service__head">
+            Website Development
+          </div>
+          <ul className='service__list'>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+          </ul>
+        </article>
+
+
+
+        
+
+
+        {/* ------------------------------------------------ */}
+        <article className='service'>
+          <div className="service__head">
+           MERN stack WEB apps
+          </div>
+          <ul className='service__list'>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+          </ul>
+        </article>
+
+
+
+        
+
+
+        {/* ------------------------------------------------ */}
+        <article className='service'>
+          <div className="service__head">
+           Wordpress Website
+          </div>
+          <ul className='service__list'>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+            <li>
+              <div className="service__icon">
+              <BsCheck/>
+              </div>
+              <h5>Lorem ipsum dolor sit amet.</h5>
+            </li>
+          </ul>
+        </article>
+
+
+
+        
+
+
+        {/* ------------------------------------------------ */}
+      </div>
+    </section>
   )
 }
