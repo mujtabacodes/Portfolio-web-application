044938c282a3d53ad6d58a3eab9eabb7e1e4d08c
Mujtaba Ali <mujiroy01@gmail.com>
2022-08-06 10:29:37 +0500

    Testimonal structure and styling is complete of work on slide the testimonial

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,7 +1,77 @@
 import React from 'react'
 import './testimonials.css'
+import fi_avator from './../../assets/images/fiverr-avator.PNG'
+import up_avator from './../../assets/images/upwork-avator.PNG'
+
+
+const dataApi=[
+  {
+  id:1,
+  avator:fi_avator,
+  name:"Racha Adel  website",
+  comment:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error doloribus labore dicta? Totam alias possimus laborum? Ab accusantium eius vel aspernatur amet nisi iure! Ab officiis maiores voluptas in labore  ",
+ 
+},
+  {
+  id:2,
+  avator:up_avator,
+  name:"Racha Adel  website",
+  comment:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error doloribus labore dicta? Totam alias possimus laborum? Ab accusantium eius vel aspernatur amet nisi iure! Ab officiis maiores voluptas in labore  ",
+ 
+},
+  {
+  id:3,
+  avator:fi_avator,
+  name:"Racha Adel  website",
+  comment:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error doloribus labore dicta? Totam alias possimus laborum? Ab accusantium eius vel aspernatur amet nisi iure! Ab officiis maiores voluptas in labore  ",
+ 
+},
+  {
+  id:4,
+  avator:up_avator,
+  name:"Racha Adel  website",
+  comment:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error doloribus labore dicta? Totam alias possimus laborum? Ab accusantium eius vel aspernatur amet nisi iure! Ab officiis maiores voluptas in labore  ",
+ 
+},
+
+
+]
+
+
 export default function Testimonials() {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <div className="container testimonials__container">
+      {
+        dataApi.map(({id,avator,name,comment})=>{
+          return(
+            <article key={id} className="testimonial">
+            <div className="client__avator">
+              <img src={avator} alt="" />
+            </div>
+            <div>
+              <h5 className="client__name">
+                {name}
+              </h5>
+              <small className="client__comment">{comment}</small>
+            </div>
+          </article>
+  
+          )
+        })
+      }
+
+        {/* ========================== */}
+
+     
+
+
+        {/* ========================== */}
+      </div>
+
+
+    </section>
   )
 }
