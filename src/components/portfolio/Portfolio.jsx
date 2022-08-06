b40e5765111337a0f761a0cfc12a8b88b7e5f72b
Mujtaba Ali <mujiroy01@gmail.com>
2022-08-06 09:10:30 +0500

    Portfolio structure is done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,71 @@
 import React from 'react'
 import './portfolio.css'
+import porfolioimg from './../../assets/images/home-img.jpg';
 export default function Portfolio() {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+      <article className='porfolio__item'>
+        <div className="portfolio__item-img">
+          <img src={porfolioimg} alt="" />
+        </div>
+        <h3>This is portfolio</h3>
+        <a href="http://www.google.com" className='btn' target='_blank'>GitHub</a>
+        <a href="http://www.google.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+        
+      </article>
+
+
+      {/* ========================================= */}
+      <article className='porfolio__item'>
+        <div className="portfolio__item-img">
+          <img src={porfolioimg} alt="" />
+        </div>
+        <h3>This is portfolio</h3>
+        <a href="http://www.google.com" className='btn' target='_blank'>GitHub</a>
+        <a href="http://www.google.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+        
+      </article>
+      <article className='porfolio__item'>
+        <div className="portfolio__item-img">
+          <img src={porfolioimg} alt="" />
+        </div>
+        <h3>This is portfolio</h3>
+        <a href="http://www.google.com" className='btn' target='_blank'>GitHub</a>
+        <a href="http://www.google.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+        
+      </article>
+      <article className='porfolio__item'>
+        <div className="portfolio__item-img">
+          <img src={porfolioimg} alt="" />
+        </div>
+        <h3>This is portfolio</h3>
+        <a href="http://www.google.com" className='btn' target='_blank'>GitHub</a>
+        <a href="http://www.google.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+        
+      </article>
+      <article className='porfolio__item'>
+        <div className="portfolio__item-img">
+          <img src={porfolioimg} alt="" />
+        </div>
+        <h3>This is portfolio</h3>
+        <a href="http://www.google.com" className='btn' target='_blank'>GitHub</a>
+        <a href="http://www.google.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+        
+      </article>
+      <article className='porfolio__item'>
+        <div className="portfolio__item-img">
+          <img src={porfolioimg} alt="" />
+        </div>
+        <h3>This is portfolio</h3>
+        <a href="http://www.google.com" className='btn' target='_blank'>GitHub</a>
+        <a href="http://www.google.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+        
+      </article>
+      {/* ========================================= */}
+      </div>
+    </section>
   )
 }
