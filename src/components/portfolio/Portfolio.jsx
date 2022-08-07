fee20898ccc81fb43e1c31a410e9b9429616b999
Mujtaba Ali <mujiroy01@gmail.com>
2022-08-07 12:55:30 +0500

    Woring on console error

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,82 +1,108 @@
-import React from 'react'
-import './portfolio.css'
-import porfolioimg from './../../assets/images/home-img.jpg';
+import React, { useRef, useState } from "react";
+import "./portfolio.css";
+import porfolioimg from "./../../assets/images/home-img.jpg";
 
-const dataApi=[
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+const dataApi = [
   {
-  id:1,
-  image:porfolioimg,
-  title:"Racha Adel  website",
-  github:'https://github.com',
-  demo:'https://www.google.com'
-},
+    id: 1,
+    image: porfolioimg,
+    title: "Racha Adel  website",
+    github: "https://github.com",
+    demo: "https://www.google.com",
+  },
   {
-  id:2,
-  image:porfolioimg,
-  title:"Racha Adel  website",
-  github:'https://github.com',
-  demo:'https://www.google.com'
-},
+    id: 2,
+    image: porfolioimg,
+    title: "Racha Adel  website",
+    github: "https://github.com",
+    demo: "https://www.google.com",
+  },
   {
-  id:3,
-  image:porfolioimg,
-  title:"Racha Adel  website",
-  github:'https://github.com',
-  demo:'https://www.google.com'
-},
+    id: 3,
+    image: porfolioimg,
+    title: "Racha Adel  website",
+    github: "https://github.com",
+    demo: "https://www.google.com",
+  },
   {
-  id:4,
-  image:porfolioimg,
-  title:"Racha Adel  website",
-  github:'https://github.com',
-  demo:'https://www.google.com'
-},
-
-
-
-
-]
-
+    id: 4,
+    image: porfolioimg,
+    title: "Racha Adel  website",
+    github: "https://github.com",
+    demo: "https://www.google.com",
+  },
+];
 
 export default function Portfolio() {
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
-      <div className="container portfolio__container">
-      
+      <Swiper
+        className="container portfolio__container mySwiper"
+        // loop={true}  
+       
+      //  spaceBetween={30}
+        // centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
 
-      {
-        dataApi.map(({id,image,title,github,demo})=>{
-          return(
-            <article key={id}  className='porfolio__item'>
-        <div className="portfolio__item-img">
-          <img src={image} alt={title} />
-        </div>
-        <h3>{title}</h3>
-      <div className="portfolio__item-cta">
-      <a href={github} className='btn' target='_blank'>GitHub</a>
-        <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
-        
-      </div>
-      </article>
-          )
-        })
-      }
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          }}}
 
+      >
+        {dataApi.map(({ id, image, title, github, demo }) => {
+          return (
+            <SwiperSlide key={id} className="porfolio__item">
+              <div className="portfolio__item-img">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="_blank">
+                  GitHub
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </SwiperSlide>
+          );
+        })}
 
-      {/* ========================================= */}
-    
+        {/* ========================================= */}
 
-
-
-
-
-
-
-
-      {/* ========================================= */}
-      </div>
+        {/* ========================================= */}
+      </Swiper>
     </section>
-  )
+  );
 }
