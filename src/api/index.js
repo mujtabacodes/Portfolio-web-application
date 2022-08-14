e5821372d23077429a80d569dccd5e3cfb9f271d
Mujtaba Ali <mujiroy01@gmail.com>
2022-08-14 12:14:01 +0500

    Update BaseURL to deployed Backend

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const baseURL='http://localhost:5000';
+// const baseURL='http://localhost:5000';
+const baseURL='https://mujtaba-resume.herokuapp.com/';
 // ========================portfolio===============================
 const addportfolio=baseURL+'/api/portfolio/add';
 const getportfolio=baseURL+'/api/portfolio/get';
