685518e93fcb82dd0e7eb091ca1a4ec8c309e36e
Mujtaba Ali <mujiroy01@gmail.com>
2022-08-15 19:14:52 +0500

    Update fixed testionials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import "./testimonials.css";
-import fi_avator from "./../../assets/images/fiverr-avator.PNG";
-import up_avator from "./../../assets/images/upwork-avator.PNG";
+import fi_avator from "./../../assets/images/fiverr_avator.png";
+import up_avator from "./../../assets/images/upwork_avator.png";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -18,30 +18,37 @@ const FixedData = [
   {
     id: 1,
     avator: fi_avator,
-    name: "Racha Adel  website",
+    name: "sis1397_sl",
     comment:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error doloribus labore dicta? Totam alias possimus laborum? Ab accusantium eius vel aspernatur amet nisi iure! Ab officiis maiores voluptas in labore  ",
+    "Mujtaba has been great to work with, he has helped me throughout the whole process and definitely is superb at what he does. I am very excited to run the site myself now and with the tutorial guide on how to manage my site is very helpful. I would definitely recommend Mujtaba to anyone who is looking for a professional website to be made. Thank You so much Mujtaba!",
   },
   {
     id: 2,
     avator: up_avator,
-    name: "Racha Adel  website",
+    name: "Racha Adel",
     comment:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error doloribus labore dicta? Totam alias possimus laborum? Ab accusantium eius vel aspernatur amet nisi iure! Ab officiis maiores voluptas in labore  ",
+    "Excellent. Such a pleasure to work with someone who is responsive and has a lot of skills, as well as patience. I enjoyed working with Shafique Mujtaba. He was very hardworking and provided great suggestions to make sure my website exactly how I imagined. I absolutely recommend hiring him . 'If you looking for a true professional this guy is your guy! I guarantee you wouldn’t be disappointed!'",
   },
   {
     id: 3,
     avator: fi_avator,
-    name: "Racha Adel  website",
+    name: "Letlotlompete",
     comment:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error doloribus labore dicta? Totam alias possimus laborum? Ab accusantium eius vel aspernatur amet nisi iure! Ab officiis maiores voluptas in labore  ",
+    "The service was seemless. He communicates all the time and the work was exceptional and punctual. I am extremely satisfied with the work he has done me!!!",
   },
   {
     id: 4,
-    avator: up_avator,
-    name: "Racha Adel  website",
+    avator:  fi_avator,
+    name: "Jefowles",
     comment:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error doloribus labore dicta? Totam alias possimus laborum? Ab accusantium eius vel aspernatur amet nisi iure! Ab officiis maiores voluptas in labore  ",
+      "polite and easy to communicate with.",
+  },
+  {
+    id: 5,
+    avator:  fi_avator,
+    name: "Purebliss",
+    comment:
+      "The best I've ever worked with! Great communication! will work with again! Super kind person. Thank you so much!",
   },
 ];
 
